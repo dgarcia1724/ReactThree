a78059e1303c0986e50365a4d36cc92390ccf28f
dgarcia1724 <dgarcia1724@berkeley.edu>
2023-07-26 11:14:02 -0700

    start

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header />
+      <Body />
+      <Footer />
     </div>
   );
+}
+
+function Header() {
+  return (
+    <header>
+      <h1>Hi lol :D</h1>
+      <p>(logo here)</p>
+    </header>
+  );
+}
+
+function Body() {
+  return (
+    <main>
+      <Pizza />
+      <h2>This is a thing.</h2>
+    </main>
+  );
+}
+
+function Pizza() {
+  return (
+    <div>
+      <img src="pizzas/spinaci.jpg" alt="lol" />
+      <img src="pizzas/spinaci.jpg" alt="Pizza spinaci" />
+    </div>
+  );
+}
+
+function Footer() {
+  return <h1>Footer lol</h1>;
 }
 
 export default App;
